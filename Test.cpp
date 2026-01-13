c8c4bfae0eb2ca60156ddcbcd89e5693e8aa0804
Mango-peraon <931406429@qq.com>
2026-01-13 20:10:40 +0800

    Test3

--- Test.cpp
+++ Test.cpp
@@ -4,5 +4,6 @@ int main(void)
     using namespace std;
     cout << "Hello World!" << endl;
     cout << "Let's test RedPitaya!" << endl;
+    cout << "C++ language standard version: This project uses the __cplusplus language standard version." << endl;
     return 0;
 }
